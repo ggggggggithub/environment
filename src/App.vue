9439095459bc85617df806335513d893c27037e9
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-11-06 03:11:45 +0900

    Test: vuetify test용 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-btn color="primary">Hello Vuetify!</v-btn>
+    </v-container>
+  </v-app>
+</template>
+
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
 
 <style scoped>
 header {
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import vuetify from './plugin/vuetify.js'
+//import vuetify from './plugin/vuetify.js'
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(vuetify, createPinia())
+//app.use(vuetify)
+app.use(pinia)
 
 app.mount('#app')
