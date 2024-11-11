10dbd3be58fc22c54616abc8d2b3f5437efc91a7
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-11-11 22:02:03 +0900

    Wip : env 작업중

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -24,6 +24,50 @@ function loadMap() {
 
   map = new window.kakao.maps.Map(container, options)
 
+  function displayMarker(locPosition: any, message: any) {
+    // 마커를 생성합니다
+    const marker = new kakao.maps.Marker({
+      map: map,
+      position: locPosition,
+    })
+
+    const iwContent = message, // 인포윈도우에 표시할 내용
+      iwRemoveable = true
+
+    // 인포윈도우를 생성합니다
+    const infowindow = new kakao.maps.InfoWindow({
+      content: iwContent,
+      removable: iwRemoveable,
+    })
+
+    // 인포윈도우를 마커위에 표시합니다
+    infowindow.open(map, marker)
+
+    // 지도 중심좌표를 접속위치로 변경합니다
+    map.setCenter(locPosition)
+  }
+
+  if (navigator.geolocation) {
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = position.coords.latitude, // 위도
+        lon = position.coords.longitude // 경도
+
+      const locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        message = '<div style="padding:5px;">여기에 계신가요?!</div>' // 인포윈도우에 표시될 내용입니다
+
+      // 마커와 인포윈도우를 표시합니다
+      displayMarker(locPosition, message)
+    })
+  } else {
+    // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+    const locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+      message = 'geolocation을 사용할수 없어요..'
+
+    displayMarker(locPosition, message)
+  }
+
   const positions = [
     {
       title: '카카오',
@@ -67,8 +111,7 @@ function loadMap() {
 // 카카오 스크립트를 동적으로 추가하는 함수
 function loadScript() {
   const script = document.createElement('script')
-  script.src =
-    '//dapi.kakao.com/v2/maps/sdk.js?appkey=ce7bb9e14c3037277539e74278ef75fd&autoload=false'
+  script.src = `${process.env.VUE_APP_KAKAO}`
   script.onload = () => window.kakao.maps.load(loadMap)
 
   document.head.appendChild(script)
