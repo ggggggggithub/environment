f2942afe698431538fac8551494316b966009611
ggggggggithub <hamtoli5@gmail.com>
2024-11-14 18:22:29 +0900

    feat: mapmarker

--- src/components/NaverMap.vue
+++ src/components/NaverMap.vue
@@ -1,8 +1,10 @@
+
 <template>
-  <div id="map" style="width: 100%; height: 500px;"></div>
+  <div id="map" style="width: 90%; height: 500px; margin-top:80px;"></div>
 </template>
 
 <script setup lang="ts">
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { onMounted } from 'vue';
 
 onMounted(() => {
@@ -13,12 +15,14 @@ onMounted(() => {
   };
 
   const map = new naver.maps.Map('map', mapOptions); // 'map' ID로 지도 초기화
+
+  const marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.5665, 126.9780), // 마커를 표시할 위치 (예: 서울)
+    map: map,
+    title: '네이버 지도 마커',
+  })
 });
+
+
 </script>
 
-<style scoped>
-#map {
-  width: 100%;
-  height: 500px;
-}
-</style>
